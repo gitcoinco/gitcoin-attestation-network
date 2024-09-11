65e21dd71a0d148d6f624fd96be0472332e327a0
Nick Lionis <nicklioniss@gmail.com>
2024-09-11 10:26:04 +0300

    used eas contracts package

--- contracts/contracts/GitcoinGrantsResolver.sol
+++ contracts/contracts/GitcoinGrantsResolver.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
-import {IEAS, Attestation} from "./EAS/IEAS.sol";
-import {SchemaResolver} from "./EAS/resolver/SchemaResolver.sol";
+import {IEAS, Attestation} from "@ethereum-attestation-service/eas-contracts/contracts/IEAS.sol";
+import {SchemaResolver} from "@ethereum-attestation-service/eas-contracts/contracts/resolver/SchemaResolver.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
