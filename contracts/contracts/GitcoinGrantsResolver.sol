ec22acaccab0ba3748bb73dfac49c778e09347d1
Aditya Anand M C <aditya.anandmc@gmail.com>
2024-09-20 10:48:19 +0530

    improvments/1 (#2)
    * - remove valid prefix
    - add batch operation to delegator
    - add/update comments
    - add treasury address
    
    * add + run prettier
    
    * update package.json
    
    * fix compile errors
    
    * fixes
    
    ---------
    
    Co-authored-by: 0xKurt <Kurt@Merbeth.io>

--- contracts/contracts/GitcoinGrantsResolver.sol
+++ contracts/contracts/GitcoinGrantsResolver.sol
@@ -1,6 +1,7 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.22;
 
+// External Libraries
 import {SchemaResolver} from "@ethereum-attestation-service/eas-contracts/contracts/resolver/SchemaResolver.sol";
 import {IEAS, Attestation} from "@ethereum-attestation-service/eas-contracts/contracts/IEAS.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
@@ -14,40 +15,75 @@ import {SafeTransferLib} from "solady/src/utils/SafeTransferLib.sol";
 contract GitcoinGrantsResolver is SchemaResolver, AccessControl {
     using SafeTransferLib for address;
 
+    // ===============
+    // === Errors ====
+    // ===============
+
     error UnauthorizedAttester();
     error NotDelegatorsManager();
     error NotAdmin();
+    error ZeroAddress();
 
-    /// @notice Role for valid delegators
-    bytes32 public constant VALID_DELEGATOR_ROLE = keccak256("VALID_DELEGATOR_ROLE");
-    
-    /// @notice Role for managing the valid delegators
-    bytes32 public constant DELEGATORS_MANAGER_ROLE = keccak256("DELEGATORS_MANAGER_ROLE");
-    
+    // ==================
+    // === Constants ====
+    // ==================
+
+    /// @notice Role for delegators
+    bytes32 public constant DELEGATOR_ROLE = keccak256("DELEGATOR_ROLE");
+
+    /// @notice Role for managing the delegators
+    bytes32 public constant DELEGATORS_MANAGER_ROLE =
+        keccak256("DELEGATORS_MANAGER_ROLE");
+
     /// @notice Address of the native token
     address public constant NATIVE = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
+    // ==================
+    // === Variables ====
+    // ==================
+
+    address public treasury;
+
+    // ==================
+    // ===== Events =====
+    // ==================
+
+    /// @notice Emitted when the treasury address is updated.
+    /// @param treasury The address of the treasury.
+    event TreasuryUpdated(address indexed treasury);
+
+    /// ====================================
+    /// ========== Constructor =============
+    /// ====================================
+
     /**
      * @dev Constructor to initialize the contract with essential parameters.
-     * @param admin The address of the contract admin (owner).
-     * @param manager The manager of the contract delegators.
-     * @param delegators The list of initial valid delegators.
+     * @param _eas The address of the EAS contract.
+     * @param _admin The address of the contract admin (owner).
+     * @param _manager The manager of the contract delegators.
+     * @param _delegators The list of initial valid delegators.
+     * @param _treasury The address of the treasury.
      */
     constructor(
-        IEAS eas,
-        address admin,
-        address manager,
-        address[] memory delegators
-    ) SchemaResolver(eas) {
+        IEAS _eas,
+        address _admin,
+        address _manager,
+        address[] memory _delegators,
+        address _treasury
+    ) SchemaResolver(_eas) {
         // Set the admin role to the provided admin address
-        _grantRole(DEFAULT_ADMIN_ROLE, admin);
-        _grantRole(DELEGATORS_MANAGER_ROLE, manager);
+        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+        _grantRole(DELEGATORS_MANAGER_ROLE, _manager);
 
-        // Assign the VALID_DELEGATOR_ROLE to initial delegators
-        for (uint256 i = 0; i < delegators.length; i++) {
-            _grantRole(VALID_DELEGATOR_ROLE, delegators[i]);
-        }
+        // Assign the DELEGATOR_ROLE to initial delegators
+        addDelegators(_delegators);
+
+        updateTreasury(_treasury);
     }
+
+    /// ====================================
+    /// ======= External / Public ==========
+    /// ====================================
 
     /**
      * @dev Handles attestation by validating the attester.
@@ -59,7 +95,7 @@ contract GitcoinGrantsResolver is SchemaResolver, AccessControl {
         uint256 /* value */
     ) internal view override returns (bool) {
         address attester = attestation.attester;
-        if (!hasRole(VALID_DELEGATOR_ROLE, attester)) {
+        if (!hasRole(DELEGATOR_ROLE, attester)) {
             revert UnauthorizedAttester();
         }
 
@@ -86,25 +122,46 @@ contract GitcoinGrantsResolver is SchemaResolver, AccessControl {
     }
 
     /**
-     * @notice Adds a new valid delegator.
-     * @param delegator The address of the new valid delegator.
+     * @notice Add delegators.
+     * @param _delegators An array of addresses representing the delegators to be added.
      */
-    function addValidDelegator(address delegator) external {
-        if (!hasRole(DELEGATORS_MANAGER_ROLE, msg.sender)) {
+    function addDelegators(address[] memory _delegators) public {
+        if (!hasRole(DELEGATORS_MANAGER_ROLE, msg.sender))
             revert NotDelegatorsManager();
+
+        uint256 length = _delegators.length;
+        for (uint256 i = 0; i < length; i++) {
+            _addDelegator(_delegators[i]);
         }
-        _grantRole(VALID_DELEGATOR_ROLE, delegator);
     }
 
     /**
-     * @notice Removes a valid delegator.
-     * @param delegator The address of the valid delegator to be removed.
+     * @notice Remove delegators.
+     * @param _delegators An array of addresses representing the delegators to be removed.
      */
-    function removeValidDelegator(address delegator) external {
-        if (!hasRole(DELEGATORS_MANAGER_ROLE, msg.sender)) {
+    function removeDelegators(address[] memory _delegators) public {
+        if (!hasRole(DELEGATORS_MANAGER_ROLE, msg.sender))
             revert NotDelegatorsManager();
+
+        uint256 length = _delegators.length;
+        for (uint256 i = 0; i < length; i++) {
+            _removeDelegator(_delegators[i]);
         }
-        _revokeRole(VALID_DELEGATOR_ROLE, delegator);
+    }
+
+    /**
+     * @notice Updates the treasury address.
+     * @param _treasury The address of the treasury.
+     */
+    function updateTreasury(address _treasury) public {
+        if (!hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) {
+            revert NotAdmin();
+        }
+        if (_treasury == address(0)) {
+            revert ZeroAddress();
+        }
+        treasury = _treasury;
+        emit TreasuryUpdated(treasury);
     }
 
     /**
@@ -113,16 +170,16 @@ contract GitcoinGrantsResolver is SchemaResolver, AccessControl {
      * @param _to The address to which the balance will be transferred.
      * @param _amount The amount to be transferred.
      */
-    function withdraw(
-        address _token, 
-        address _to, 
-        uint256 _amount
-    ) external {
+    function withdraw(address _token, address _to, uint256 _amount) external {
         if (!hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) {
             revert NotAdmin();
         }
         transferAmount(_token, _to, _amount);
     }
+
+    /// ====================================
+    /// ======= Internal / Private =========
+    /// ====================================
 
     /**
      * @notice Transfer an amount of a token to an address
@@ -130,11 +187,34 @@ contract GitcoinGrantsResolver is SchemaResolver, AccessControl {
      * @param _to The address to transfer to
      * @param _amount The amount to transfer
      */
-    function transferAmount(address _token, address _to, uint256 _amount) internal {
+    function transferAmount(
+        address _token,
+        address _to,
+        uint256 _amount
+    ) internal {
         if (_token == NATIVE) {
             _to.safeTransferETH(_amount);
         } else {
             _token.safeTransfer(_to, _amount);
         }
+    }
+
+    /**
+     * @notice Adds a new valid delegator.
+     * @param _delegator The address of the new valid delegator.
+     */
+    function _addDelegator(address _delegator) private {
+        if(_delegator == address(0)) {
+            revert ZeroAddress();
+        }
+        _grantRole(DELEGATOR_ROLE, _delegator);
+    }
+
+    /**
+     * @notice Removes a delegator.
+     * @param _delegator The address of the valid delegator to be removed.
+     */
+    function _removeDelegator(address _delegator) private {
+        _revokeRole(DELEGATOR_ROLE, _delegator);
     }
 }
